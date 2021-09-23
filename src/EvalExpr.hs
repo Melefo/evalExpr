21d669c42ac598210e1f4b3c514ed1fba1ad0d6f
Melefo <melefo@melefo.xyz>
2021-09-23 12:05:27 +0200

    Removing whitespace

--- src/EvalExpr.hs
+++ src/EvalExpr.hs
@@ -9,8 +9,12 @@ module EvalExpr where
 
 import Control.Exception
 import Errors
+import Data.List (intercalate)
+
+removeWhitespace :: String -> String
+removeWhitespace x = intercalate "" $ words x
 
 evalExpr :: [String] -> IO ()
 evalExpr [] = throw NoArg
-evalExpr [x] = putStrLn "Hello World"
+evalExpr [x] = putStrLn $ removeWhitespace x
 evalExpr (x:_) = throw TooManyArg

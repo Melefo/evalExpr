be04c65bdedad21bd2f2e2ad3fa394b28a35de8a
Melefo <melefo@melefo.xyz>
2021-09-24 10:08:35 +0200

    Adding exercice 1.3.1

--- src/EvalExpr.hs
+++ src/EvalExpr.hs
@@ -9,12 +9,28 @@ module EvalExpr where
 
 import Control.Exception
 import Errors
-import Data.List (intercalate)
+import Data.List
+import Data.Maybe
 
-removeWhitespace :: String -> String
-removeWhitespace x = intercalate "" $ words x
+type Parser a = String -> Maybe (a , String)
+
+parseChar :: Char -> Parser Char
+parseChar c (x:xs)
+    | c == x = Just(c, xs)
+    | otherwise = Nothing
+
+parseAnyChar :: String -> Parser Char
+parseAnyChar [] _ = Nothing
+parseAnyChar (x:xs) str
+    | isNothing $ parseChar x str = parseAnyChar xs str
+    | otherwise = Just (x, tail str)
+
+parseOr :: Parser a -> Parser a -> Parser a
+parseOr p1 p2 str 
+    | isNothing $ p1 str = p2 str
+    | otherwise = p1 str
 
 evalExpr :: [String] -> IO ()
 evalExpr [] = throw NoArg
-evalExpr [x] = putStrLn $ removeWhitespace x
+evalExpr [x] = putStrLn x
 evalExpr (x:_) = throw TooManyArg

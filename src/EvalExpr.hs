3df60da8ba4eb6f6dc2aad6fad71d26cd4f69330
Melefo <melefo@melefo.xyz>
2021-09-27 09:36:15 +0200

    Adding parseMany

--- src/EvalExpr.hs
+++ src/EvalExpr.hs
@@ -27,7 +27,7 @@ parseAnyChar (x:xs) str
 
 parseOr :: Parser a -> Parser a -> Parser a
 parseOr _ _ [] = Nothing
-parseOr p1 p2 str 
+parseOr p1 p2 str
     | isNothing $ p1 str = p2 str
     | otherwise = p1 str
 
@@ -36,7 +36,7 @@ parseAnd _ _ [] = Nothing
 parseAnd p1 p2 str
     | isNothing $ p1 str = Nothing
     | isNothing $ p2 $ tail str = Nothing
-    | otherwise = 
+    | otherwise =
         case p1 str of
             Just (a, b) -> case p2 b of
                 Just (c, d) -> Just ((a,c), d)
@@ -47,33 +47,18 @@ parseAndWith :: (a -> b -> c) -> Parser a -> Parser b -> Parser c
 parseAndWith _ _ _ [] = Nothing
 parseAndWith func p1 p2 str
     | isNothing $ parseAnd p1 p2 str = Nothing
-    | otherwise = 
+    | otherwise =
         case parseAnd p1 p2 str of
             Just ((c1, c2), result) -> Just (func c1 c2, result)
             _  -> Nothing
 
---fusion :: [Maybe  (a, String)] -> Maybe ([a], String)
---fusion [x] = Nothing
---fusion xs = 
-    --let Just test = last xs
-    --in Just (foldl (\ x (Just z) -> x ++ [fst z]) [] xs, snd test)
-
---           String -> Maybe (a , String) -> Parser [a]
---parseMany :: Parser a -> Parser [a]
---parseMany _ [] = Nothing
---parseMany p str = fusion [p str]
-
 parseMany :: Parser a -> Parser [a]
-parseMany Nothing = 
-
-testtt :: Parser a -> Parser [a]
-testtt _ [] = Nothing
-testtt p str = 
-    let parse = p str
-    in case parse of
+parseMany p str = 
+    case p str of
         Just (c, rest) ->
-            let recursive = testtt p rest
-            in case 
+            case parseMany p rest of
+                Just (c2, rest2) -> Just (c : c2, rest2)
+                _ -> Just ([c], rest)
         _ -> Nothing
 
 evalExpr :: [String] -> IO ()
